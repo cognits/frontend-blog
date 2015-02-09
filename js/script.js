2953200905667a8ca0a56d3ffb200231aaf967b1
KAR2014 <kherrera16@gmail.com>
2015-02-09 16:36:57 -0600

    Ya jale el json y lo agregue a la pagina

--- js/script.js
+++ js/script.js
@@ -11,9 +11,17 @@ $(document).ready(function () {
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
     
-    $(".posts1Title").html(posts['posts'][0]['title']);
-    $(".posts1Date").html(posts['posts'][0]['date']);
-    $(".posts1Content").html(posts['posts'][0]['content']);
+    $(".postsTitle1").html(posts['posts'][0]['title']);
+    $(".postsContent1").html(posts['posts'][0]['content']);
+    $(".postsDate1").html(posts['posts'][0]['date']);
+
+    $(".postsTitle2").html(posts['posts'][1]['title']);
+    $(".postsContent2").html(posts['posts'][1]['content']);
+    $(".postsDate2").html(posts['posts'][1]['date']);
+
+    $(".postsTitle3").html(posts['posts'][2]['title']);
+    $(".postsContent3").html(posts['posts'][2]['content']);
+    $(".postsDate3").html(posts['posts'][2]['date']);
     //
   });
 });
