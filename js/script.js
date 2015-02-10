176bb114420c9fb797b4cd5997418c05c61593e5
KAR2014 <kherrera16@gmail.com>
2015-02-10 16:49:50 -0600

    Modifique codigo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 
 // Javascript Code.
-$(document).ready(function () {
+$(document).ready(function() {
   // $.get("_posts/2014-11-09.json", function(data){
   //
   //   var posts = JSON.parse(data);
   //   $(".posts").html(posts['posts'][0]['title']);
   //
   // });
-
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
     
@@ -24,4 +23,5 @@ $(document).ready(function () {
     $(".postsDate3").html(posts['posts'][2]['date']);
     //
   });
+  $("#menu").accordion();
 });
