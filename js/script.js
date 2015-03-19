e84d3d75054837348e98020cdf001941b1e1ac16
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-03-19 16:55:48 -0600

    primeros cambios al repocitorio del blog

--- js/script.js
+++ js/script.js
@@ -9,7 +9,20 @@ $(document).ready(function () {
 
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
-    $(".posts").html(posts['posts'][0]['title']);
+    $(".posts").html(posts['posts'][0]['content']);
+    $(".posts1").html(posts['posts'][0]['title']);
+    $(".posts2").html(posts['posts'][0]['date']);
+    //
+  });
+});
+
+
+$(document).ready(function () {
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".face").html(posts['posts'][1]['content']);
+    $(".face1").html(posts['posts'][1]['title']);
+    $(".face2").html(posts['posts'][1]['date']);
     //
   });
 });
