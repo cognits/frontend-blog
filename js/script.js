f33c71654492248a6a8b58828ad19bf7feeb2e16
Marvin Abel <marvinabelmon@gmail.com>
2015-03-19 16:55:31 -0600

    En esta version se añaden los posts en el documento Json y se hacen visibles en el html

--- js/script.js
+++ js/script.js
@@ -9,7 +9,11 @@ $(document).ready(function () {
 
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
-    $(".posts").html(posts['posts'][0]['title']);
+    $(".title").html(posts['posts'][0]['title'])
+    $(".posts").html(posts['posts'][0]['content']);
+    $(".posts2").html(posts['posts'][1]['content']);
+    $(".posts3").html(posts['posts'][2]['content']);
+    $(".posts4").html(posts['posts'][3]['content'])
     //
   });
 });
