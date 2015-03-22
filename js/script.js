7dbb7d5e3289e5b89208090e5b738b3cef54c834
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-03-22 17:46:13 -0600

    los post estan integrados

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Javascript Code.
 
-//Esto es para la caja de busqueda
+//This is the search box on the page
 $(document).ready(function(){
     $('#button').click(function() {
         var toAdd = $('input[name=checkListItem]').val();   
@@ -8,44 +8,14 @@ $(document).ready(function(){
     });
 });
 
-//esto es para que se oculte la caja
+//This is the search box on the page
 $(document).ready(function(){
     $('.pull-me').click(function(){
         $(".panel").slideToggle('slow');
     });
 });
 
-
-//aqui estan los Json
-$(document).ready(function () {
-  // $.get("_posts/2014-11-09.json", function(data){
-  //
-  //   var posts = JSON.parse(data);
-  //   $(".posts").html(posts['posts'][0]['title']);
-  //
-  // });
-  $(this).load("_posts/posts.json", function(data){
-    var posts = JSON.parse(data);
-    $(".posts").html(posts['posts'][0]['content']);
-    $(".posts1").html(posts['posts'][0]['title']);
-    $(".posts2").html(posts['posts'][0]['date']);
-    //
-  });
-});
-
-
-$(document).ready(function () {
-  $(this).load("_posts/posts.json", function(data){
-    var posts = JSON.parse(data);
-    $(".face").html(posts['posts'][1]['content']);
-    $(".face1").html(posts['posts'][1]['title']);
-    $(".face2").html(posts['posts'][1]['date']);
-    //
-  });
-});
-
-
-//Para resaltar el texto !!!! 
+//This is the search box on the page!!!! 
 jQuery.fn.extend({
     resaltar: function(busqueda, claseCSSbusqueda){
         var regex = new RegExp("(<[^>]*>)|("+ busqueda.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1") +')', 'ig');
@@ -55,7 +25,167 @@ jQuery.fn.extend({
         return nuevoHtml;
     }
 });
-
+//To highlight text
 function resaltarTexto(){
     $("#texto").resaltar(cajaTexto.value, "resaltarTexto");
 }
+//end tehe search box
+
+
+
+//Here are the Json
+$(document).ready(function () {
+  // $.get("_posts/2014-11-09.json", function(data){
+  //
+  //   var posts = JSON.parse(data);
+  //   $(".posts").html(posts['posts'][0]['title']);
+  //
+  // });
+
+//The first post
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".title").html(posts['posts'][0]['title']);
+    $(".content").html(posts['posts'][0]['content']);
+    $(".date").html(posts['posts'][0]['date']);
+
+    //
+  });
+});
+//The image of first post
+$(function(){
+ $(document).ready(function(){
+  $.getJSON("_posts/posts.json",
+   function(data){
+    $.each(data.posts, function(i,auto){
+     $("<img/>").attr("src", auto.image).appendTo("#imagenes");
+    });
+   });
+  return false;
+ });
+});
+
+//the second post
+$(document).ready(function () {
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".titles").html(posts['posts'][1]['title']);
+    $(".contents").html(posts['posts'][1]['content']);
+    $(".dates").html(posts['posts'][1]['date']);
+    //
+  });
+});
+//The image of second post
+$(function(){
+ $(document).ready(function(){
+  $.getJSON("_posts/posts.json",
+   function(data){
+    $.each(data.posts, function(i,auto){
+     $("<img/>").attr("src", auto.image1).appendTo("#imagene");
+    });
+   });
+  return false;
+ });
+});
+
+//the third posts
+$(document).ready(function () {
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".titless").html(posts['posts'][2]['title']);
+    $(".contentss").html(posts['posts'][2]['content']);
+    $(".datess").html(posts['posts'][2]['date']);
+    //
+  });
+});
+//The image of third post
+$(function(){
+ $(document).ready(function(){
+  $.getJSON("_posts/posts.json",
+   function(data){
+    $.each(data.posts, function(i,auto){
+     $("<img/>").attr("src", auto.image2).appendTo("#imageness");
+    });
+   });
+  return false;
+ });
+});
+
+//the fourth post
+$(document).ready(function () {
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".title4").html(posts['posts'][3]['title']);
+    $(".content4").html(posts['posts'][3]['content']);
+    $(".date4").html(posts['posts'][3]['date']);
+    //
+  });
+});
+//The image of fourth post
+$(function(){
+ $(document).ready(function(){
+  $.getJSON("_posts/posts.json",
+   function(data){
+    $.each(data.posts, function(i,auto){
+     $("<img/>").attr("src", auto.image3).appendTo("#imagen4");
+    });
+   });
+  return false;
+ });
+});
+
+//The fifth post
+$(document).ready(function () {
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".title5").html(posts['posts'][4]['title']);
+    $(".content5").html(posts['posts'][4]['content']);
+    $(".date5").html(posts['posts'][4]['date']);
+    //
+  });
+});
+//The image of fifth post
+$(function(){
+ $(document).ready(function(){
+  $.getJSON("_posts/posts.json",
+   function(data){
+    $.each(data.posts, function(i,auto){
+     $("<img/>").attr("src", auto.image4).appendTo("#imagen5");
+    });
+   });
+  return false;
+ });
+});
+
+//The sixth post
+$(document).ready(function () {
+  $(this).load("_posts/posts.json", function(data){
+    var posts = JSON.parse(data);
+    $(".title6").html(posts['posts'][5]['title']);
+    $(".content6").html(posts['posts'][5]['content']);
+    $(".date6").html(posts['posts'][5]['date']);
+    //
+  });
+});
+//The image of sixth post
+$(function(){
+ $(document).ready(function(){
+  $.getJSON("_posts/posts.json",
+   function(data){
+    $.each(data.posts, function(i,auto){
+     $("<img/>").attr("src", auto.image5).appendTo("#imagen6");
+    });
+   });
+  return false;
+ });
+});
+//en the post
+
+
+$(document).ready(function(){
+    $(".more").toggle(function() {
+        $(this).text("Leer menos...").siblings(".complete").show();
+    }, function() {
+        $(this).text("Leer mas...").siblings(".complete").hide();
+    }); 
+});
