6e6ef25c88d4337a621b5c67ac1ae2f64923b7c3
Marvin Abel <marvinabelmon@gmail.com>
2015-03-28 18:28:17 -0600

    En esta version se quitan los botones que tenian el titulo de los json, se añade el buscador de palabras, y se borra el texto del campo de texto a la hora de publicar un post

--- js/script.js
+++ js/script.js
@@ -12,8 +12,9 @@ $(document).ready(function () {
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
 
-    function order_date (a, b) {
-      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    //
+    function order_date (date1, date2) {
+      return new Date(date2.date).getTime() - new Date(date1.date).getTime();
     }
 
     posts['posts'].sort(order_date);
@@ -107,6 +108,8 @@ $(document).ready(function () {
         var to_add_comment = $("#comment_input1").val();
         if (to_add_comment != '' && to_add_name != '') {
           append('#comment_div1',to_add_name,to_add_comment);
+        $("input[name='name_input1']").val("");
+        $("#comment_input1").val("");
         };
     });
 
@@ -116,6 +119,8 @@ $(document).ready(function () {
         if (to_add_comment != '' && to_add_name != '') {
           append('#comment_div2',to_add_name,to_add_comment);
         };
+        $("input[name='name_input2']").val("");
+        $("#comment_input2").val("");
     });
 
     $("#save_comment3").click(function() {
@@ -123,6 +128,8 @@ $(document).ready(function () {
         var to_add_comment = $("#comment_input3").val();
         if (to_add_comment != '' && to_add_name != '') {
           append('#comment_div3',to_add_name,to_add_comment);
+        $("input[name='name_input3']").val("");
+        $("#comment_input3").val("");
         };
     });
 
@@ -131,41 +138,39 @@ $(document).ready(function () {
         var to_add_comment = $("#comment_input4").val();
         if (to_add_comment != '' && to_add_name != '') {
           append('#comment_div4',to_add_name,to_add_comment);
+        $("input[name='name_input4']").val("");
+        $("#comment_input4").val("");
         };
     });
 
     var append = function(target, input_name, input_comment) {
-      $(target).append('<div class=user_comment>' + '<b>' + input_name + ': </b>' + input_comment + '</div>');
+      $(target).append('<p class=user_comment>' + '<b>' + input_name + ': </b>' + input_comment + '</p>');
     };
+
+    $(".search_button").click(function() {
+      $('.to_search').val("");
+      $(".search_container").slideToggle();
+      $('.lista p').removeClass('highlight');
+      $('.lista h1').removeClass('highlight');
+    });
+
+    $.expr[':'].icontains = function(obj, index, meta, stack){
+    return (obj.textContent || obj.innerText || jQuery(obj).text() || '').toLowerCase().indexOf(meta[3].toLowerCase()) >= 0;
+    };
+
+    $(document).ready(function(){   
+        $('#to_search').keyup(function(){
+                     buscar = $(this).val();
+                     $('.lista h1').removeClass('highlight');
+                     $('.lista p').removeClass('highlight');
+
+                            if(jQuery.trim(buscar) != ''){
+                               $(".lista p:icontains('" + buscar + "')").addClass('highlight');
+                               $(".lista h1:icontains('" + buscar + "')").addClass('highlight');
+                            }
+        });
+    });
+
   });
 });
 
-
-$(document).ready(function(){
-    $('#buttonsearch').click(function() {
-        var toAdd = $('input[name=checkListItem]').val();   
-        $('.list').append('<div class="item">' + toAdd + '</div>')
-    });
-});
-
-//This is the search box on the page
-$(document).ready(function(){
-    $('.pull-me').click(function(){
-        $(".panelsearch").slideToggle('slow');
-    });
-});
-//This is the search box on the page!!!! 
-jQuery.fn.extend({
-    resaltar: function(busqueda, claseCSSbusqueda){
-        var regex = new RegExp("(<[^>]*>)|("+ busqueda.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1") +')', 'ig');
-        var nuevoHtml=this.html(this.html().replace(regex, function(a, b, c){
-            return (a.charAt(0) == "<") ? a : "<span class=\""+ claseCSSbusqueda +"\">" + c + "</span>";
-        }));
-        return nuevoHtml;
-    }
-});
-//To highlight text
-
-function resaltarTexto(){
-    $("body").resaltar(cajaTexto.value, "resaltarTexto");
-}
